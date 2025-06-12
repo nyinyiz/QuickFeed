bb86e57abe5488e6b22ed881310fcc6637265ff4
Nyi Nyi Zaw <nyinyizaw.dev@gmail.com>
2025-06-12 22:03:43 +0700

    refactor: Clean up Color.kt by removing unnecessary comments

--- app/src/main/java/com/nyinyi/quickfeed/ui/theme/Color.kt
+++ app/src/main/java/com/nyinyi/quickfeed/ui/theme/Color.kt
@@ -2,18 +2,17 @@ package com.nyinyi.quickfeed.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Color.kt
-val Primary = Color(0xFF1E88E5) // Vivid Blue
+val Primary = Color(0xFF1E88E5)
 val OnPrimary = Color.White
-val PrimaryContainer = Color(0xFFCCE4FD) // Light blue container
+val PrimaryContainer = Color(0xFFCCE4FD)
 
-val Secondary = Color(0xFFFBC02D) // Butter Yellow
+val Secondary = Color(0xFFFBC02D)
 val OnSecondary = Color.Black
-val SecondaryContainer = Color(0xFFFFF8E1) // Light yellow container
+val SecondaryContainer = Color(0xFFFFF8E1)
 
-val Tertiary = Color(0xFF5C6BC0) // Indigo
+val Tertiary = Color(0xFF5C6BC0)
 val OnTertiary = Color.White
-val TertiaryContainer = Color(0xFFD8DADC) // Light indigo container
+val TertiaryContainer = Color(0xFFD8DADC)
 
 val Background = Color(0xFFF5F5F5)
 val OnBackground = Color(0xFF212121)
